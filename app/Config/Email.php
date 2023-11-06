aeabbd6c7f019e70f93f3832b450a0d2b26f404a
PeiHan Zhu <0116zxdjboy@gmail.com>
2023-11-06 09:29:24 +0800

    chore: composer require codeigniter4/shield:dev-develop.

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = '0116zxdjboy@gmail.com';
+    public string $fromName   = 'Han';
     public string $recipients = '';
 
     /**
